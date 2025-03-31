d1221f8746425b5e1e9dfb1550d108765f513b2c
George Nosachev <nosachev.gosha@gmail.com>
2025-03-31 22:43:03 +0500

    fix: commented because of no need after direct db connection

--- src/Services/ProductService/ProductService.API/Data/ApplicationDbContext.cs
+++ src/Services/ProductService/ProductService.API/Data/ApplicationDbContext.cs
@@ -1,20 +1,20 @@
-using System.Collections.Generic;
-using MongoDB.Driver;
-using ProductService.API.Models;
+// using System.Collections.Generic;
+// using MongoDB.Driver;
+// using ProductService.API.Models;
 
-namespace ProductService.API.Data
-{
-    public class ApplicationDbContext
-    {
-        private readonly IMongoDatabase _database;
+// namespace ProductService.API.Data
+// {
+//     public class ApplicationDbContext
+//     {
+//         private readonly IMongoDatabase _database;
 
-        public ApplicationDbContext(IMongoClient mongoClient, string databaseName)
-        {
-            _database = mongoClient.GetDatabase(databaseName);
-        }
+//         public ApplicationDbContext(IMongoClient mongoClient, string databaseName)
+//         {
+//             _database = mongoClient.GetDatabase(databaseName);
+//         }
 
-        public IMongoCollection<Product> Products => _database.GetCollection<Product>("Products");
-        public IMongoCollection<Category> Categories =>
-            _database.GetCollection<Category>("Categories");
-    }
-}
+//         public IMongoCollection<Product> Products => _database.GetCollection<Product>("Products");
+//         public IMongoCollection<Category> Categories =>
+//             _database.GetCollection<Category>("Categories");
+//     }
+// }
